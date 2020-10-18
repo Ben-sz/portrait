29f790c1c774aca4e52e4bb784015bcb1c4254c2
Ben-sz <sz.bence53@gmail.com>
2020-10-18 20:50:12 +0200

    adds animation to ribbon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CocolioComponent } from './cocolio/cocolio.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CocolioComponent
+    CocolioComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
