59c3ba9fb263b6540ee8f94decf4a5276836001e
Ben-sz <sz.bence53@gmail.com>
2020-10-25 20:46:10 +0100

    adds links and contacts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { faGithub, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  faGithub = faGithub;
+  faLinkedinIn = faLinkedinIn;
 
   constructor() { }
 
