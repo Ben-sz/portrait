096fd5fd4778c4c05b31e2693d52a06d75874ee8
Ben-sz <sz.bence53@gmail.com>
2020-11-12 15:35:29 +0100

    adds links and contacts scrolling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   scrollHtml: HTMLElement;
   @ViewChild('portrait') portraitElement: any;
   portraitHtml: HTMLElement;
+
   @ViewChild('xpLeft') xpLeft: any;
   xpLeftHtml: HTMLElement;
   @ViewChild('xpRight') xpRight: any;
@@ -24,6 +25,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   projectsLeftHtml: HTMLElement;
   @ViewChild('projectsRight') projectsRight: any;
   projectsRightHtml: HTMLElement;
+
+  @ViewChild('linksLeft') linksLeft: any;
+  linksLeftHtml: HTMLElement;
+  @ViewChild('linksRight') linksRight: any;
+  linksRightHtml: HTMLElement;
+
+
+  @ViewChild('contactsLeft') contactsLeft: any;
+  contactsLeftHtml: HTMLElement;
+  @ViewChild('contactsRight') contactsRight: any;
+  contactsRightHtml: HTMLElement;
+
 
   startX: number;
 
@@ -53,6 +66,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.xpRightHtml = this.xpRight.nativeElement;
     this.projectsLeftHtml = this.projectsLeft.nativeElement;
     this.projectsRightHtml = this.projectsRight.nativeElement;
+    this.contactsLeftHtml = this.contactsLeft.nativeElement;
+    this.contactsRightHtml = this.contactsRight.nativeElement;
+    this.linksLeftHtml = this.linksLeft.nativeElement;
+    this.linksRightHtml = this.linksRight.nativeElement;
 
 
     console.log('translate3d('+ window.innerWidth/2 + 'px,0px,0px)')
@@ -67,9 +84,9 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     this.xpRightHtml.style.transform = 'translate3d('+ this.fadeValue + 'px,0px,0px)';
     this.xpLeftHtml.style.transform = 'translate3d('+ (-this.fadeValue) + 'px,0px,0px)';
-/* 
+ 
     this.projectsRightHtml.style.transform = 'translate3d('+ window.innerWidth/2 + 'px,0px,0px)';
-    this.projectsLeftHtml.style.transform = 'translate3d('+ (-window.innerWidth) + 'px,0px,0px)'; */
+    this.projectsLeftHtml.style.transform = 'translate3d('+ (-window.innerWidth) + 'px,0px,0px)'; 
     this.startX = this.xpRightHtml.getBoundingClientRect().y - this.windowHeight;
     console.log('start',  this.startX )
 
@@ -104,15 +121,12 @@ export class HomeComponent implements OnInit, OnDestroy {
     /*    *2 a scrolling speed*/
     /* this.startX is used to reset to default when scrolling bac */
 
-
     if (this.fadeValue > 475){
       var xpRateLeft = (Math.max(this.fadeValue - n*2, 0)) -(Math.min((this.fadeValue-150 + (xpBotoutOfView)*1.2), 0));
     }
     else{
       var xpRateLeft = (Math.max(((this.fadeValue + (xpTopInView))), 0)) -(Math.min((this.fadeValue-150 + (xpBotoutOfView)*1.2), 0))
     }
-
-  
 
 
     this.xpRightHtml.style.transform = 'translate3d(' + xpRateLeft + 'px,0px,0px)';
@@ -125,9 +139,33 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 
     var projectsRateRight = (Math.max((this.fadeValue + (projectsTopInView)*1.2), 0)) -(Math.min(( (projectsBotOutofView-200)*1.2 ), 0) );
-     this.projectsRightHtml.style.transform = 'translate3d(' + projectsRateRight + 'px,0px,0px)';
-      this.projectsLeftHtml.style.transform = 'translate3d(' + -projectsRateRight + 'px,0px,0px)';
+    this.projectsRightHtml.style.transform = 'translate3d(' + projectsRateRight + 'px,0px,0px)';
+    this.projectsLeftHtml.style.transform = 'translate3d(' + -projectsRateRight + 'px,0px,0px)';
  
+    /* contacts scroll */
+
+
+    let contactsBounding = this.contactsLeftHtml.getBoundingClientRect();
+    let contactsTopInView = contactsBounding.y-window.innerHeight;
+    let contactsBotOutofView = contactsBounding.y+contactsBounding.height;
+
+
+    var contactsRateRight = (Math.max((this.fadeValue + (contactsTopInView)*1.2), 0)) -(Math.min(( (contactsBotOutofView-200)*1.2 ), 0) );
+    this.contactsRightHtml.style.transform = 'translate3d(' + contactsRateRight + 'px,0px,0px)';
+    this.contactsLeftHtml.style.transform = 'translate3d(' + -contactsRateRight + 'px,0px,0px)';
+
+    /* links scroll */
+
+
+    let linksBounding = this.linksLeftHtml.getBoundingClientRect();
+    let linksTopInView = linksBounding.y-window.innerHeight;
+    let linksBotOutofView = linksBounding.y+linksBounding.height;
+
+
+    var linksRateRight = (Math.max((this.fadeValue + (linksTopInView)*1.2), 0)) -(Math.min(( (linksBotOutofView-200)*1.2 ), 0) );
+    this.linksRightHtml.style.transform = 'translate3d(' + linksRateRight + 'px,0px,0px)';
+    this.linksLeftHtml.style.transform = 'translate3d(' + -linksRateRight + 'px,0px,0px)';
+
   
   } 
 
